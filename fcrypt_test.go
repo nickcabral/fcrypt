1cc595374e7c34a8dcaf8d82b4dcb05d0d3e48ed
Nick Cabral <nick@freshlytyped.io>
2018-06-22 23:31:54 -0600

    fix decryption benchmarks and improve timing

--- fcrypt_test.go
+++ fcrypt_test.go
@@ -1,7 +1,6 @@
 package fcrypt
 
 import (
-	"crypto/rand"
 	"io/ioutil"
 	"os"
 	"path"
@@ -34,27 +33,24 @@ func teardown() {
 }
 
 func TestInit_NonExistentPath_Err(t *testing.T) {
-	_, err := Init(pw, unknowndir)
-
-	if err == nil {
+	setup()
+	if _, err := Init(pw, unknowndir); err == nil {
 		t.Error()
 	}
 	teardown()
 }
 
 func TestInit_EmptyPassword_NoErr(t *testing.T) {
-	_, err := Init("", encrypteddir)
-
-	if err != nil {
+	setup()
+	if _, err := Init("", encrypteddir); err != nil {
 		t.Error(err)
 	}
 	teardown()
 }
 
 func TestInit_GoodPath_NoErr(t *testing.T) {
-	_, err := Init(pw, encrypteddir)
-
-	if err != nil {
+	setup()
+	if _, err := Init(pw, encrypteddir); err != nil {
 		t.Error(err)
 	}
 	teardown()
@@ -62,7 +58,6 @@ func TestInit_GoodPath_NoErr(t *testing.T) {
 
 func TestLoad_InitializedDir_NoErr(t *testing.T) {
 	setup()
-
 	if _, err := Load(encrypteddir); err != nil {
 		t.Error(err)
 	}
@@ -71,7 +66,6 @@ func TestLoad_InitializedDir_NoErr(t *testing.T) {
 
 func TestLoad_UninitializedDir_Err(t *testing.T) {
 	setup()
-
 	if _, err := Load(unknowndir); err == nil {
 		t.Error()
 	}
@@ -80,9 +74,7 @@ func TestLoad_UninitializedDir_Err(t *testing.T) {
 
 func TestConfig_Encrypt_FileExists_NoErr(t *testing.T) {
 	setup()
-
 	err := cfg.Encrypt(existingfile)
-
 	if err != nil {
 		t.Error(err)
 	}
@@ -91,9 +83,7 @@ func TestConfig_Encrypt_FileExists_NoErr(t *testing.T) {
 
 func TestConfig_Encrypt_FileExists_ExpectedEncryptedDataExists(t *testing.T) {
 	setup()
-
 	_ = cfg.Encrypt(existingfile)
-
 	if _, err := os.Stat(encrypteddatafile); err != nil {
 		t.Error(err)
 	}
@@ -102,7 +92,6 @@ func TestConfig_Encrypt_FileExists_ExpectedEncryptedDataExists(t *testing.T) {
 
 func TestConfig_Encrypt_FileDoesntExist_Err(t *testing.T) {
 	setup()
-
 	if err := cfg.Encrypt("someotherfile"); err == nil {
 		t.Error()
 	}
@@ -112,7 +101,6 @@ func TestConfig_Encrypt_FileDoesntExist_Err(t *testing.T) {
 func TestConfig_Decrypt_KnownFileGoodPassword_NoErr(t *testing.T) {
 	setup()
 	_ = cfg.Encrypt(existingfile)
-
 	if err := cfg.Decrypt(pw, existingfile); err != nil {
 		t.Error(err)
 	}
@@ -122,9 +110,7 @@ func TestConfig_Decrypt_KnownFileGoodPassword_NoErr(t *testing.T) {
 func TestConfig_Decrypt_KnownFileGoodPassword_ExpectedDataRestored(t *testing.T) {
 	setup()
 	_ = cfg.Encrypt(existingfile)
-
 	_ = cfg.Decrypt(pw, existingfile)
-
 	if _, err := os.Stat(existingfile); err != nil {
 		t.Error(err)
 	}
@@ -137,9 +123,7 @@ func TestConfig_Decrypt_KnownFileGoodPassword_ExpectedDataRestored(t *testing.T)
 func TestConfig_Decrypt_KnownFileBadPassword_DataNotDecrypted(t *testing.T) {
 	setup()
 	_ = cfg.Encrypt(existingfile)
-
 	_ = cfg.Decrypt("wrong password", existingfile)
-
 	if _, err := os.Stat(existingfile); err == nil {
 		t.Error()
 	}
@@ -149,111 +133,72 @@ func TestConfig_Decrypt_KnownFileBadPassword_DataNotDecrypted(t *testing.T) {
 func TestConfig_Decrypt_KnownFileBadPassword_Err(t *testing.T) {
 	setup()
 	_ = cfg.Encrypt(existingfile)
-
 	if err := cfg.Decrypt("no good", existingfile); err == nil {
 		t.Error()
 	}
 	teardown()
 }
 
-func BenchmarkEncrypt_4KBFile(b *testing.B) {
-	setup()
-	content := make([]byte, 1<<12)
-	rand.Read(content)
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		b.StopTimer()
-		ioutil.WriteFile(existingfile, []byte(content), os.ModePerm)
-		b.StartTimer()
-
-		cfg.Encrypt(existingfile)
-	}
-	b.StopTimer()
-	teardown()
+func BenchmarkEncrypt_64BFile(b *testing.B) {
+	benchmarkEncrypt(b, 1<<6)
 }
 
-func BenchmarkEncrypt_1MBFile(b *testing.B) {
-	setup()
-	content := make([]byte, 1<<20)
-	rand.Read(content)
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		b.StopTimer()
-		ioutil.WriteFile(existingfile, []byte(content), os.ModePerm)
-		b.StartTimer()
-
-		cfg.Encrypt(existingfile)
-	}
-	b.StopTimer()
-	teardown()
+func BenchmarkEncrypt_16KBFile(b *testing.B) {
+	benchmarkEncrypt(b, 1<<14)
 }
 
-func BenchmarkEncrypt_256MBFile(b *testing.B) {
-	setup()
-	content := make([]byte, 1<<28)
-	rand.Read(content)
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		b.StopTimer()
-		ioutil.WriteFile(existingfile, []byte(content), os.ModePerm)
-		b.StartTimer()
-
-		cfg.Encrypt(existingfile)
-	}
-	b.StopTimer()
-	teardown()
+func BenchmarkEncrypt_4MBFile(b *testing.B) {
+	benchmarkEncrypt(b, 1<<22)
 }
 
-func BenchmarkDecrypt_4KBFile(b *testing.B) {
-	setup()
-	content := make([]byte, 1<<12)
-	rand.Read(content)
-	b.ResetTimer()
+func BenchmarkEncrypt_1GBFile(b *testing.B) {
+	benchmarkEncrypt(b, 1<<30)
+}
 
+func BenchmarkDecrypt_64BFile(b *testing.B) {
+	benchmarkDecrypt(b, 1<<6)
+}
+
+func BenchmarkDecrypt_16KBFile(b *testing.B) {
+	benchmarkDecrypt(b, 1<<14)
+}
+
+func BenchmarkDecrypt_4MBFile(b *testing.B) {
+	benchmarkDecrypt(b, 1<<22)
+}
+
+func BenchmarkDecrypt_1GBFile(b *testing.B) {
+	benchmarkDecrypt(b, 1<<30)
+}
+
+func benchmarkEncrypt(b *testing.B, dataLength uint64) {
+	setupBenchmark(b, dataLength)
 	for i := 0; i < b.N; i++ {
-		b.StopTimer()
-		cfg.Encrypt(existingfile)
 		b.StartTimer()
+		cfg.Encrypt(existingfile)
+		b.StopTimer()
 
 		cfg.Decrypt(pw, existingfile)
 	}
-	b.StopTimer()
 	teardown()
 }
 
-func BenchmarkDecrypt_1MBFile(b *testing.B) {
-	setup()
-	content := make([]byte, 1<<20)
-	rand.Read(content)
-	b.ResetTimer()
-
+func benchmarkDecrypt(b *testing.B, dataLength uint64) {
+	setupBenchmark(b, dataLength)
 	for i := 0; i < b.N; i++ {
-		b.StopTimer()
 		cfg.Encrypt(existingfile)
-		b.StartTimer()
 
+		b.StartTimer()
 		cfg.Decrypt(pw, existingfile)
+		b.StopTimer()
 	}
-	b.StopTimer()
 	teardown()
 }
 
-func BenchmarkDecrypt_256MBFile(b *testing.B) {
-	setup()
-	content := make([]byte, 1<<28)
-	rand.Read(content)
-	b.ResetTimer()
-
-	for i := 0; i < b.N; i++ {
-		b.StopTimer()
-		cfg.Encrypt(existingfile)
-		b.StartTimer()
-
-		cfg.Decrypt(pw, existingfile)
-	}
+func setupBenchmark(b *testing.B, dataLength uint64) {
 	b.StopTimer()
-	teardown()
+	b.ResetTimer()
+	setup()
+	content := make([]byte, dataLength)
+	ioutil.WriteFile(existingfile, []byte(content), os.ModePerm)
 }
